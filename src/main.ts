849a2826993512a662b30c643d180b340be48be9
Aaron <larryscape21@gmail.com>
2023-06-22 09:05:12 +0200

    inlcuding styles in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { createApp } from "vue";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import VueMultiselect from 'vue-multiselect'
+import "@vuepic/vue-datepicker/dist/main.css";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
+import "cropperjs/dist/cropper.css";
 
 import App from "./App.vue";
 import router from "./router";
